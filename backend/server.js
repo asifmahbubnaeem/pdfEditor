9118ecb74692292b52dd21fd398d4065c6153281
asifmahbubnaeem <asifnaim0123@gmail.com>
2025-09-06 22:19:06 +0600

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ import Redis from "ioredis";
 import  fs from "fs";
 import  fsp from "fs/promises";
 import  https from "https";
-import { PDFDocument } from 'pdf-lib';
+import { PDFDocument, degrees } from 'pdf-lib';
+// import { router } from './auth';
 
 const app = express();
 const upload = multer({ dest: "uploads/" });
@@ -41,13 +42,15 @@ const sslOptions={
   cert: fs.readFileSync('server.cert')
 };
 
-const allowedOrigin = "http://localhost:5173";
+const allowedOrigin = ["http://localhost:5173", "http://localhost:4173"];
 app.use(cors({
     origin: allowedOrigin,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json());
+
+// app.use("/api/auth", authRoutes);
 
 // app.use(cors);
 
@@ -57,7 +60,6 @@ const WINDOW_SEC = 60;
 const rateLimiter = async (req, res, next) => {
   try {
     const userKey = req.body.userId;
-    console.log("inside rate limiter, req.body.userId = ", userKey)
     const count = await redis.incr(userKey);
 
     if (count === 1) {
@@ -138,6 +140,12 @@ function getDeletedPages(deletedPagesAsStr){
 }
 
 
+function getNumericOrder(pageOrder){
+  let arr = pageOrder.trim().split(',');
+  const numArr=arr.map(Number);
+  return numArr;
+}
+
 app.post("/api/delete-pages", uploadToDelete.single("file"), rateLimiter, async(req, res) =>{
 
     try{
@@ -174,15 +182,127 @@ app.post("/api/delete-pages", uploadToDelete.single("file"), rateLimiter, async(
       fs.writeFileSync(outputPath, pdfBytes);
 
       res.download(outputPath, outputPath, (err) => {
-            if(err)console.log("Error downloading img to pdf converted file: ",outputPath);
-            fs.unlinkSync(outputPath);
+            if(err)console.log("Error downloading updated (with deleted page) pdf file: ",outputPath);
+            // fs.unlinkSync(outputPath);
         });
-      console.log("downloaded updated pdf");
+      console.log("downloaded updated (with deleted pages) pdf");
 
     }catch(err){
       console.error("Detele action error: ",err);
       res.status(500).json({error: "Failed to perform delete action."})
     }
+});
+
+function getPageNumbers(pageNum){
+  if(pageNum.length==0)
+    return [];
+  const arr = pageNum.split(',');
+  return arr.map(Number);
+}
+
+
+function reConstructMap(strRotation){
+
+  const parsedArray = JSON.parse(strRotation);
+  const reconstructedMap = new Map(parsedArray);
+
+  return reconstructedMap;
+}
+
+app.post("/api/page-del-rotation", uploadToDelete.single("file"),rateLimiter, async (req, res) => {
+
+    try{
+
+      if(!req.file){
+        return res.status(400).json({error: "No input file found"});
+      }
+        const inputPath = req.file.path;
+        const outputDir = path.join(process.cwd(), "rotation_action");
+        const outputPath = path.join(outputDir,`rotation_update_${Date.now()}.pdf`);
+
+
+        const rotationInfo =  reConstructMap(req.body.rotationInfo);
+        const deletedPageNo =  getPageNumbers(req.body.deletedPage);
+
+        const existingPdfBytes = await req.file.buffer;
+        const pdfDoc = await PDFDocument.load(existingPdfBytes);
+        const newPdf = await PDFDocument.create();
+
+        const pageCount = pdfDoc.getPageCount();
+
+        for(let i=0; i<pageCount; i++)
+        {
+          let pg_num = i+1;
+          if(!deletedPageNo.includes(pg_num)){
+            const [copiedPage] = await newPdf.copyPages(pdfDoc,[i]);
+
+            if(rotationInfo.has(pg_num))
+              copiedPage.setRotation(degrees(rotationInfo.get(pg_num)));
+            newPdf.addPage(copiedPage);
+          }
+
+        }
+
+        const pdfBytes = await newPdf.save();
+
+
+        if(!fs.existsSync(outputDir))
+          fs.mkdirSync(outputDir);
+        fs.writeFileSync(outputPath, pdfBytes);
+
+
+        res.download(outputPath, outputPath, (err) => { 
+            if(err)
+              console.log("Error downloading pdf file ", outputPath);
+        });
+        console.log("Download successful.")
+
+    }catch(err){
+      console.error("Rotation delete action error: ",err);
+      res.status(500).json({error: "Failed to process rotation/delete update."})
+    }
+
+});
+
+app.post("/api/page-rearrange", uploadToDelete.single("file"), rateLimiter, async (req, res) => {
+
+  try{
+
+    if(!req.file){
+      return res.status(400).json({error: "No input file found"});
+    }
+
+    const inputPath = req.file.path;
+    const outputDir = path.join(process.cwd(), "reorder_action");
+    const outputPath = path.join(outputDir,`reorder_update_${Date.now()}.pdf`);
+
+    const pageOrder = getNumericOrder(req.body.newPageOrder);
+    const existingPdfBytes = await req.file.buffer;
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    const newPdf = await PDFDocument.create();
+
+    const copiedPages = await newPdf.copyPages(
+        pdfDoc,
+        pageOrder.map((n) => n - 1) // convert to 0-based index
+    );
+
+    copiedPages.forEach((page) => newPdf.addPage(page));
+    const pdfBytes = await newPdf.save();
+
+    if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+      fs.writeFileSync(outputPath, pdfBytes);
+
+    res.download(outputPath, outputPath, (err) => {
+          if(err)console.log("Error downloading reordered pdf file: ",outputPath);
+          fs.unlinkSync(outputPath);
+      });
+    console.log("downloaded reordered updated pdf");
+
+  }catch(err){
+    console.error("Re-arrange action error: ", err);
+    res.status(500).json({error: "Failed to re-arrange action"});
+  }
+
 });
 
 app.post("/api/convert-img", upload.single("file"), rateLimiter, async(req, res) =>{
